47b99faae133787b36446d725c0d366bdf526992
lily.cherry <lily.cherry@kakaocorp.com>
2021-10-06 15:32:52 +0900

    2.2 Delete To Do

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const DELETE_TODO = "DELETE_TODO"
 const reducer = (state = [], action) => {
   switch (action.type){
     case ADD_TODO:
-      return [...state, { text: action.text, date: Date.now() }]
+      return [...state, { text: action.text, id: Date.now() }]
     case DELETE_TODO:
       return []
     default:
@@ -19,13 +19,46 @@ const reducer = (state = [], action) => {
 }
 
 const store = createStore(reducer)
-store.subscribe(()=>console.log(store.getState()))
+
+const addTodo = text => {
+  return { type: ADD_TODO, text }
+}
+
+const dispatchAddToDo = text => {
+  store.dispatch(addTodo(text))
+}
+
+const deleteTodo = id => {
+  return { type: DELETE_TODO, id }
+}
+
+const dispatchDeleteToDo = (e) => {
+  const id = e.target.parentNode.id
+  store.dispatch(deleteTodo(id))
+}
+
+const paintToDos = () => {
+  const toDos = store.getState()
+  ul.innerHTML = ""
+  toDos.forEach(toDo => {
+    const li = document.createElement("li")
+    const btn = document.createElement("button")
+    btn.innerText = "DEL"
+    btn.addEventListener("click", dispatchDeleteToDo)
+    li.id = toDo.id
+    li.innerText = toDo.text
+    li.appendChild(btn)
+    ul.appendChild(li)
+  })
+}
+
+store.subscribe(paintToDos)
 
 const onSubmit = e => {
   e.preventDefault()
   const toDo = input.value
   input.value = ""
-  store.dispatch({ type: ADD_TODO, text: toDo })
+  dispatchAddToDo(toDo)
 }
 
 form.addEventListener("submit", onSubmit)
