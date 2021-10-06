17787214707fad2ed4382356bea8f07765cea532
lily.cherry <lily.cherry@kakaocorp.com>
2021-10-06 14:23:39 +0900

    #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ const add = document.getElementById("add")
 const minus = document.getElementById("minus")
 const number = document.querySelector("span")
 
-const countModifier = (state = 0) => {
-  return state
+const countModifier = (count = 0, action) => {
+  if(action.type === "ADD"){
+    return count + 1
+  }
+  else if(action.type === "MINUS"){
+    return count - 1
+  }
+  return count
 }
 
 const countStroe = createStore(countModifier)
+
+countStroe.dispatch({ type : "HELLO"})
