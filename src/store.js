71599b1591c2cf39475200e12374ad34fea74af4
lily.cherry <cbr5964@naver.com>
2021-10-07 10:06:15 +0900

    4.3 ConfigureStore

--- src/store.js
+++ src/store.js
@@ -1,5 +1,4 @@
-import { createStore } from "redux";
-import { createAction, createReducer } from "@reduxjs/toolkit"
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit"
 
 const addToDo = createAction("ADD")
 const deleteToDo = createAction("DELETE")
@@ -17,7 +16,7 @@ const reducer = createReducer([], {
         state.filter(toDo => (toDo.id !== action.payload))
 })
 
-const store = createStore(reducer)
+const store = configureStore({ reducer })
 
 export const actionCreators = {
     addToDo,
