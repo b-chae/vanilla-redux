002d62ed0e926ff983c4dfaaa22e5947fe60bde0
lily.cherry <cbr5964@naver.com>
2021-10-07 09:41:13 +0900

    3.5 Detail Screen

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,19 @@
-export default () => "Detail"
+import React from "react";
+import { connect } from "react-redux";
+
+function Detail({toDos}){
+    return (
+        <>
+            <h1>{toDos?.text}</h1>
+            <h5>Created at {Date(toDos.id)}</h5>
+        </>
+    )
+}
+
+function mapStateToProps(state, ownProps){
+    const {match: {params: {id}}} = ownProps
+
+    return {toDos: state.find(toDo => toDo.id === parseInt(id))}
+}
+
+export default connect(mapStateToProps)(Detail)
